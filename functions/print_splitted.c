5865aa86d64c3786762cb94a999de98ce339cf45
Stijn Appunn <sappunn@student.codam.nl>
2022-02-14 18:21:30 +0100

    Fixed header and comments

--- functions/print_splitted.c
+++ functions/print_splitted.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   get_working_directory.c                                  :+:    :+:      */
+/*   print_splitted.c                                    :+:    :+:           */
 /*                                                     +:+                    */
 /*   By: sappunn <sappunn@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
@@ -12,9 +12,8 @@
 
 #include "../libft/libft.h"
 #include "../headers/functions.h"
-#include "../headers/structs.h"
 
-/*
+/**
 * Takes an char ptr path
 * If path == NULL we get the current working directory and return it as a string.
 * Else we return path, esentially functioning as a setter
@@ -23,7 +22,6 @@
 *
 * @return	a path, either current working dir or what we set it to be
 */
-
 void	print_splitted(char **args)
 {
 	int	i;
